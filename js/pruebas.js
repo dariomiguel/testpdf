6fa80f14168ab3368993a1b012c83acceb3dc2e2
Darío Miguel <dario.e.miguel@gmail.com>
2024-12-03 14:22:27 +0000

    checkbox disabled

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,5 +1,16 @@
 const checkboxTimeFaena = document.getElementById("actividadEnFanea");
+const checkboxTimeDespostada = document.getElementById("actividadEnDespostada");
+const timeContainerFaena = document.getElementById("timeContainerFaena");
+const timeContainerDespostada = document.getElementById("timeContainerDespostada");
+const ingresoFaena = document.getElementById("ingresoFaena");
+const salidaFaena = document.getElementById("salidaFaena");
 
 checkboxTimeFaena.addEventListener("change", function () {
-    console.log("Hola");
+    ingresoFaena.disabled = !checkboxTimeFaena.checked;
+    salidaFaena.disabled = !checkboxTimeFaena.checked;
+});
+
+checkboxTimeDespostada.addEventListener("change", function () {
+    ingresoDespostada.disabled = !checkboxTimeDespostada.checked;
+    salidaDespostada.disabled = !checkboxTimeDespostada.checked;
 });
