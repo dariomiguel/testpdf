b62365d758b7ac8ed42123ff25bf7d7ce542d3d4
Darío Miguel <dario.e.miguel@gmail.com>
2024-12-03 17:15:04 +0000

    Se agrego boton generar que muestra con console.log las hs en minutos

--- js/pruebas.js
+++ js/pruebas.js
@@ -14,3 +14,30 @@ checkboxTimeDespostada.addEventListener("change", function () {
     ingresoDespostada.disabled = !checkboxTimeDespostada.checked;
     salidaDespostada.disabled = !checkboxTimeDespostada.checked;
 });
+
+const btnGenerar = document.getElementById("btnGenerar");
+btnGenerar.addEventListener("click", function () {
+    if (checkboxTimeFaena.checked) {
+        console.log(
+            "Ingreso Faena: " +
+                convertirHoraAMinutosDelDia(ingresoFaena.value) +
+                " Salida Faena:" +
+                convertirHoraAMinutosDelDia(salidaFaena.value)
+        );
+    }
+    if (checkboxTimeDespostada.checked) {
+        console.log(
+            "Ingreso Despostada: " +
+                convertirHoraAMinutosDelDia(ingresoDespostada.value) +
+                " Salida Despostada:" +
+                convertirHoraAMinutosDelDia(salidaDespostada.value)
+        );
+    }
+});
+
+function convertirHoraAMinutosDelDia(tiempoEnHsYMinutos) {
+    const [horas, minutos] = tiempoEnHsYMinutos.split(":");
+    let minutosDelDia = horas * 60 + +minutos;
+
+    return minutosDelDia;
+}
